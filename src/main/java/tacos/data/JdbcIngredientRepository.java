83117743321f050fe2da1426d2bae00d78a63d08
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-06-09 01:42:59 -0400

    Fix repository

--- src/main/java/tacos/data/JdbcIngredientRepository.java
+++ src/main/java/tacos/data/JdbcIngredientRepository.java
@@ -5,9 +5,11 @@ import java.sql.SQLException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import tacos.Ingredient;
 
+@Repository
 public class JdbcIngredientRepository implements IngredientRepository {
 
 	private JdbcTemplate jdbc;
