b8cb4a3574eededee8eb2608fd49266ebd66f3e1
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-06-09 22:16:12 -0400

    Page 76 - 77

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,12 +1,19 @@
 package tacos;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@Entity
 public class Ingredient {
-
+	@Id
 	private final String id;
 	private final String name;
 	private final Type type;
@@ -14,5 +21,4 @@ public class Ingredient {
 	public static enum Type {
 		WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
 	}
-
 }
